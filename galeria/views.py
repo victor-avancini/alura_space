89afb2559994414c17e334fc9790380b14db7a55
Victor Hugo Avancini <victor.avancini@hotmail.com>
2026-05-13 23:29:31 -0300

    add initial migration and create Fotografia model; update index view and template to display photo data

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {
+            "nome": "Nebulosa de Carina",
+            "legenda": "Fonte: webbtelescope.org / NASA / James Webb"
+        },
+        2: {
+            "nome": "Galáxia NGC 1079",
+            "legenda": "Fonte: nasa.org / NASA / Hubble"
+        }
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
