067bdb412262f9b762e262cdc41612a601371bbc
Victor Hugo Avancini <victor.avancini@hotmail.com>
2026-05-14 22:11:11 -0300

    feat: db and dynamic cards

--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-        1: {
-            "nome": "Nebulosa de Carina",
-            "legenda": "Fonte: webbtelescope.org / NASA / James Webb"
-        },
-        2: {
-            "nome": "Galáxia NGC 1079",
-            "legenda": "Fonte: nasa.org / NASA / Hubble"
-        }
-    }
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
