c79fe3d208b5fcbcfbea92cd0cd9cf8531738d8b
Victor Hugo Avancini <victor.avancini@hotmail.com>
2026-05-16 10:43:31 -0300

    Add migration to alter 'foto' field in Fotografia model and create buscar.html template
    - Created a new migration to modify the 'foto' field in the Fotografia model, allowing it to be blank and specifying the upload path.
    - Added a new HTML template 'buscar.html' for the gallery search page, including a banner, tags for searching, and a gallery of photographs with conditional rendering for missing images.

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,14 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- galeria/views.py
+++ galeria/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.filter(publicada=True).order_by("data_fotografia")
+    fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
+    
+    if 'buscar' in request.GET:
+        termo_busca = request.GET['buscar']
+        if termo_busca:
+            fotografias = fotografias.filter(nome__icontains=termo_busca)
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
