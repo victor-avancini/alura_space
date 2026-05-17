99de1f2d2cbe1313c633a1af63906330ae0407c0
Victor Hugo Avancini <victor.avancini@hotmail.com>
2026-05-17 10:04:40 -0300

    feat: implement message framework and update templates; refactor header and footer includes

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -69,3 +69,21 @@ class CadastroForms(forms.Form):
             }
         )
     )
+    
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+        
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("O nome de cadastro não pode conter espaços.")
+            return nome
+            
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get('senha_1')
+        senha_2 = self.cleaned_data.get('senha_2')
+        
+        if senha_1 and senha_2 and senha_1 != senha_2:
+            raise forms.ValidationError("As senhas não coincidem.")
+        return senha_2
+        
